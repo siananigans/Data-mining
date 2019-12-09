14eac7ee698cdb26f1bd26e0b2254b4b5cf61fe2
Shannon Mulgrew <shannon.mulgrew2@mail.dcu.ie>
2019-12-09 15:40:54 +0000

    Update urls.py

--- src/StudySmart/StudySmart/urls.py
+++ src/StudySmart/StudySmart/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#input all the url files here
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
 ]
